a8c8b24f32c9eaddd51e8a02249caac83ce76c8d
zahillmtu <zachary13hill@yahoo.com>
2017-12-14 16:20:07 -0500

    sql code to setup the db tables

---
+++ setupTables.sql
@@ -0,0 +1,44 @@
+# File can be run to create all of the needed tables
+USE zahill;
+CREATE TABLE Student (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    major VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id));
+
+CREATE TABLE Exam (
+    name VARCHAR(20) NOT NULL,
+    date_created DATE NOT NULL,
+    total_points INT NOT NULL,
+    PRIMARY KEY(name));
+
+CREATE TABLE Questions (
+    question_id INT NOT NULL,
+    exam_name VARCHAR(20) NOT NULL,
+    correct_choice VARCHAR(4) NOT NULL,
+    points INT NOT NULL,
+    text VARCHAR(512) NOT NULL,
+    PRIMARY KEY(question_id, exam_name));
+
+CREATE TABLE Choices (
+    choice_id VARCHAR(4) NOT NULL,
+    text VARCHAR(512) NOT NULL,
+    question_id INT NOT NULL,
+    exam_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY(choice_id, question_id, exam_name));
+
+CREATE TABLE Takes (
+    student_id INT NOT NULL,
+    exam_name VARCHAR(20) NOT NULL,
+    grade INT NOT NULL,
+    PRIMARY KEY (student_id, exam_name));
+
+CREATE TABLE Answers (
+    student_id INT NOT NULL,
+    exam_name VARCHAR(20) NOT NULL,
+    question_id INT NOT NULL,
+    student_choice INT,
+    score INT NOT NULL,
+    PRIMARY KEY (student_id, exam_name, question_id));
+
